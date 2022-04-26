52a5cc7ef2e740610ff3921903c95b8b0a8c1104
Cameron <sicklecj@gmail.com>
2022-04-26 15:36:01 -0500

    add pseudo code

--- Develop/index.js
+++ Develop/index.js
@@ -4,8 +4,28 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 
+// a high-quality, professional README.md is generated with the title of my project and sections entitled Description, 
+// Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
 // TODO: Create an array of questions for user input
 const questions = [];
+
+//Pulled question ideas from Acceptance Criteria
+
+//Ask for project title
+//Ask for project description
+
+//Ask for installation instructions
+//Ask for Usage Information
+//Ask for Contribution Guidelines
+//Ask for Test instructions
+
+
+//Ask for license from list of options
+
+//Ask for gitHub username
+//Ask for email
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
