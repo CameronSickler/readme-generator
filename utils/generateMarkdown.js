df1a5f36865a7eca5c2487c0df6a413f81638898
Cameron <sicklecj@gmail.com>
2022-04-28 10:00:32 -0500

    testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,10 +4,7 @@ function renderLicenseBadge(license) {
 
   if (license === 'APACHE') {
 
-    return `## Apache
-    ### Apache 2.0 License
-    [![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)  
-    [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
 
   } else if (license === 'BOOST') {
 
